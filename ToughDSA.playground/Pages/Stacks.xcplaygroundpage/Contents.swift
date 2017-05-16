c3e315cc8165bcf8301477c1ece25e1f885e5367
Mira Estil <imz.estil@gmail.com>
2017-05-16 00:49:48 -0400

    Stacks

--- ToughDSA.playground/Pages/Stacks.xcplaygroundpage/Contents.swift
+++ ToughDSA.playground/Pages/Stacks.xcplaygroundpage/Contents.swift
@@ -2,6 +2,50 @@
 
 import Foundation
 
-var str = "Hello, playground"
+enum Bracket: Character {
+    case Left = "("
+    case Right = ")"
+    case LeftCurly = "{"
+    case RightCurly = "}"
+    case LeftSquare = "["
+    case RightSquare = "]"
+    
+    /// For a closing bracket, the corresponding opening bracket is returned.
+    /// For an opening bracket, `nil` is returned.
+    var matchingOpen: Bracket? {
+        switch self {
+        case .Right:        return .Left
+        case .RightCurly:   return .LeftCurly
+        case .RightSquare:  return .LeftSquare
+        default:            return nil
+        }
+    }
+}
+
+func isBalanced(sequence: [Character]) -> Bool {
+    var stack = [Bracket]()
+    for char in sequence {
+        if let bracket = Bracket(rawValue: char) {
+            if let open = bracket.matchingOpen {
+                // `bracket` is a closing bracket and `open` the corresponding opening bracket:
+                guard let last = stack.last, last == open  else {
+                    return false
+                }
+                stack.removeLast()
+            } else {
+                // `bracket` is an opening bracket:
+                stack.append(bracket)
+            }
+        } else {
+            fatalError("unknown bracket found")
+        }
+    }
+    return stack.isEmpty
+}
+
+isBalanced(sequence: ["{", ")"])
+
+
+
 
 //: [Next](@next)
