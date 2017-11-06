720c9ccbfcab4ddd88b3c2b319c25e220ee120a4
Ilmira Estil <ilmira@prolificinteractive.com>
2017-11-06 15:10:40 -0500

    LinkedList

--- DSA_PPs_S.playground/Pages/PPs.xcplaygroundpage/Contents.swift
+++ DSA_PPs_S.playground/Pages/PPs.xcplaygroundpage/Contents.swift
@@ -4,6 +4,7 @@ import Foundation
 
 //https://leetcode.com/problems/two-sum/description/
 //Brute force, O(n)^2
+//nested for loops (initially check prior for array count)
 
 //Hash-Table, O(n)
 func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
@@ -34,6 +35,32 @@ func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
 //        }
 
 //twoSum([3,2,3], 6) //[0,2]
-twoSum([3,2,4], 6) //[1,2]
+twoSum ([3,2,4], 6) //[1,2]
+
+//https://leetcode.com/problems/add-two-numbers/description/
+class ListNode {
+    var val: Int
+    var next: ListNode?
+    init(_ val: Int) {
+        self.val = val
+        self.next = nil
+    }
+}
+
+func addTwoNumbers(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
+    
+    
+    return nil
+}
+
+var head = ListNode.init(1)
+head.next = ListNode.init(2)
+head.next?.next = ListNode.init(3)
+
+print(head.next!.next!.val)
+
+
+
+
 
 //: [Next](@next)
